9f1dfe657786a3a25c30e15aaa33f2bd2678af1a
fgaisboeck <GaisboeckF190055@sus.htl-grieskirchen.at>
2021-09-28 14:39:17 +0200

    4punktesweat

---
+++ src/pkg2hue/HalloJavamitForEach.java
@@ -0,0 +1,15 @@
+package pkg2hue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HalloJavamitForEach {
+
+    List<String> dieListe = new ArrayList<>();
+
+    public static void main(String[] args) {
+        for (String current : dieListe) {
+
+        }
+    }
+}
--- src/pkg2hue/Main.java
+++
@@ -1,8 +0,0 @@
-package pkg2hue;
-
-public class Main {
-
-    public static void main(String[] args) {
-    }
-
-}
